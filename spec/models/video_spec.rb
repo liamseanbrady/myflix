e74c6242427389969acb42f7780cbc7d62f9e02e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-06 22:42:31 +0000

    Add spec to test that a video can belong to a category.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -10,4 +10,15 @@ describe Video do
 
     expect(Video.first).to eq(video)
   end
+
+  it 'belongs to a category' do
+    comedies = Category.create(name: 'Comedies')
+    video = Video.new(title: 'Fringe',
+                      description: 'An awesome show',
+                      small_cover_url: '/tmp/fringe.jpg',
+                      large_cover_url: '/tmp/fringe.jpg',
+                      category: comedies)
+    
+    expect(video.category).to eq(comedies)
+  end
 end
