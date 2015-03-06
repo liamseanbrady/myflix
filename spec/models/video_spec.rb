3e69b04ea71547456b0ec16322f77a769ec6df50
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-06 23:46:17 +0000

    Add spec to test that a video must have a description in order to be saved to the database.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -21,4 +21,24 @@ describe Video do
     
     expect(video.category).to eq(comedies)
   end
+
+  it 'must have a title to be saved' do
+    comedies = Category.create(name: 'comedies')
+    video = Video.new(description: 'An awesome show',
+                      small_cover_url: '/tmp/fringe.jpg',
+                      large_cover_url: '/tmp/fringe.jpg',
+                      category: comedies)
+    
+    expect(Video.count).to eq(0)
+  end
+
+  it 'must have a description to be saved' do
+    comedies = Category.create(name: 'comedies')
+    video = Video.new(title: 'Fringe',
+                      small_cover_url: '/tmp/fringe.jpg',
+                      large_cover_url: '/tmp/fringe.jpg',
+                      category: comedies)
+       
+    expect(Video.count).to eq(0)
+  end
 end
