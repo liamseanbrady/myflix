e1654827d94223e3155011b7468a49116944edca
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-06 22:42:04 +0000

    Add spec to test that a category can have many videos.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Category do
-  
+  it 'saves itself' do
+    category = Category.new(name: 'Comedies')
+    category.save
+
+    expect(Category.first).to eq(category)
+  end
+
+  it 'has many videos' do
+    sci_fi = Category.create(name: 'Sci-fi')
+
+    fringe = Video.create(title: 'Fringe',
+                          description: 'An awesome show',
+                          small_cover_url: 'x.jpg',
+                          large_cover_url: 'x.jpg',
+                          category: sci_fi)
+    star_trek = Video.create(title: 'Fringe',
+                             description: 'An good show',
+                             small_cover_url: 'x.jpg',
+                             large_cover_url: 'x.jpg',
+                             category: sci_fi)
+                          
+    expect(sci_fi.videos).to eq([fringe, star_trek])
+  end
 end
