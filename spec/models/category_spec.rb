abd2d4f8200273f0baeb47c83570160588fd6ef4
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-06 23:50:03 +0000

    Replace rspec it/expect with shoulda matcher.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it 'has many videos' do
-    sci_fi = Category.create(name: 'Sci-fi')
-
-    fringe = Video.create(title: 'Fringe',
-                          description: 'An awesome show',
-                          small_cover_url: 'x.jpg',
-                          large_cover_url: 'x.jpg',
-                          category: sci_fi)
-    star_trek = Video.create(title: 'Fringe',
-                             description: 'An good show',
-                             small_cover_url: 'x.jpg',
-                             large_cover_url: 'x.jpg',
-                             category: sci_fi)
-                          
-    expect(sci_fi.videos).to eq([fringe, star_trek])
-  end
+  it { should have_many(:videos) }
 end
