b3460655f5da052ae70e9d0d529200d5edd47145
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-06 22:15:08 +0000

    Add spec to test that a video can be saved to the database.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Video do
-  
+  it 'saves itself' do
+    video = Video.new(title: 'Fringe',
+                      description: 'An awesome show',
+                      small_cover_url: '/tmp/fringe.jpg',
+                      large_cover_url: '/tmp/fringe.jpg')
+    video.save
+
+    expect(Video.first).to eq(video)
+  end
 end
