a5650f23df21fdc123f7dd6e905f6ad48ff3519e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-07 04:34:35 +0000

    Use shoulda matchers instead of rspec it/expect.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,34 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it 'belongs to a category' do
-    comedies = Category.create(name: 'Comedies')
-    video = Video.new(title: 'Fringe',
-                      description: 'An awesome show',
-                      small_cover_url: '/tmp/fringe.jpg',
-                      large_cover_url: '/tmp/fringe.jpg',
-                      category: comedies)
-    
-    expect(video.category).to eq(comedies)
-  end
-
-  it 'must have a title to be saved' do
-    comedies = Category.create(name: 'comedies')
-    video = Video.new(description: 'An awesome show',
-                      small_cover_url: '/tmp/fringe.jpg',
-                      large_cover_url: '/tmp/fringe.jpg',
-                      category: comedies)
-    
-    expect(Video.count).to eq(0)
-  end
-
-  it 'must have a description to be saved' do
-    comedies = Category.create(name: 'comedies')
-    video = Video.new(title: 'Fringe',
-                      small_cover_url: '/tmp/fringe.jpg',
-                      large_cover_url: '/tmp/fringe.jpg',
-                      category: comedies)
-       
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
