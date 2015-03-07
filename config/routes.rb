ab772bc53649d9efff2a178c94591ffc02ea0cbc
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-07 09:30:12 +0000

    Change method for 'search' collection route to post.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Myflix::Application.routes.draw do
 
   resources :videos, only: [:show] do
     collection do
-      get :search, to: 'videos#search'
+      post :search, to: 'videos#search'
     end
   end
   resources :categories, only: [:show]
