1962ebe72c792fa98cc24e9dccadd4032f225671
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-10 14:59:51 +0000

    Add /sign_in uri and route to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
   get '/register', to: 'users#new'
+  get '/sign_in', to: 'sessions#new'
 
   resources :videos, only: [:show] do
     collection do
