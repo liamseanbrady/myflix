38aa4b40ba56f0474fe3e1a8aa2e4f7d6e8bee13
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-10 10:56:50 +0000

    Add /register route and map to users#new

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Myflix::Application.routes.draw do
 
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
+  get '/register', to: 'users#new'
 
   resources :videos, only: [:show] do
     collection do
