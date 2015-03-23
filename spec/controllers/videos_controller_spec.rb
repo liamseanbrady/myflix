c7eb791def6e59038c98b45e3cc9bec0a17256ba
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-23 14:47:09 +0000

    Add specs for GET search

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -16,4 +16,20 @@ describe VideosController do
       expect(response).to redirect_to sign_in_path
     end
   end
+
+  describe 'GET search' do
+    it 'sets @results for authenticated users' do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+
+      get :search, search_term: video.title
+
+      expect(assigns(:results)).to match_array([video])
+    end
+    it 'redirects to sign in path for unauthenticated users' do
+      get :search, search_term: ''
+      
+      expect(response).to redirect_to sign_in_path
+    end
+  end
 end
