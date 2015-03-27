6a1f4c09ebc6abd3a1a16807890e5ad991c7f859
Liam Sean Brady <liamseanbrady@gmail.com>
2015-03-27 00:51:01 +0000

    Setup my_queue route to match to queue_items#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Myflix::Application.routes.draw do
   get '/sign_in', to: 'sessions#new'
   get '/sign_out', to: 'sessions#destroy'
 
+  get '/my_queue', to: 'queue_items#index'
+
   resources :videos, only: [:show] do
     collection do
       get :search, to: 'videos#search'
