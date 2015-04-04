cf15f12f13df33aeb75ce457f5854484489d15fc
Liam Sean Brady <liamseanbrady@gmail.com>
2015-04-04 11:57:32 -0400

    Validate that postion can only be an integer

--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe QueueItem do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:video) }
+  it { is_expected.to validate_numericality_of(:position).only_integer }
   
   describe '#video_title' do
     it 'returns the title of the associated video' do
