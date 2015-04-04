518e5674c073cc6db562bc1a3cfd9203ab75d9eb
Liam Sean Brady <liamseanbrady@gmail.com>
2015-04-04 11:00:56 -0400

    Add order by position to has many queue_items spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,5 @@ describe User do
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_presence_of(:full_name) }
   it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to have_many(:queue_items) }
+  it { is_expected.to have_many(:queue_items).order(:position) }
 end
