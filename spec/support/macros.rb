a2abec76c2dc494c7623f3a210e9900c03bbc591
Liam Sean Brady <liamseanbrady@gmail.com>
2015-04-24 12:27:45 -0400

    Minor change to set_current_usr method

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,5 +1,5 @@
-def set_current_user(user = Fabricate(:user))
-  session[:user_id] = user.id
+def set_current_user(user = nil)
+  session[:user_id] = (user || Fabricate(:user)).id
 end
 
 def clear_current_user
