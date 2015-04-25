05f07c8cb0a60cd56acd66b8c069960a1e516d07
Liam Sean Brady <liamseanbrady@gmail.com>
2015-04-25 19:43:08 -0400

    Push implementation details down to methods and bring spec to same level of abstraction

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -8,42 +8,51 @@ feature 'User interacts with queue' do
     breaking_bad = Fabricate(:video, title: 'Breaking Bad', category: tv)
     sign_in
 
-    find("a[href='/videos/#{fringe.id}']").click
-    expect(page).to have_content "#{fringe.title}"
-
-    click_link('+ My Queue')
-    expect(page).to have_content "#{fringe.title}"
+    add_video_to_queue(fringe)
+    expect_video_to_be_in_queue(fringe)
 
     visit video_path(fringe)
-    expect(page).not_to have_content '+ My Queue'
+    expect_link_not_to_be_seen('+ My Queue')
 
+    add_video_to_queue(friends)
+    add_video_to_queue(breaking_bad)
+
+    set_video_position(fringe, 2)
+    set_video_position(friends, 3)
+    set_video_position(breaking_bad, 1)
+
+    update_queue
+
+    expect_video_position(breaking_bad, 1)
+    expect_video_position(fringe, 2)
+    expect_video_position(friends, 3)
+  end
+
+  def expect_video_position(video, position)
+    expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
+  end
+
+  def set_video_position(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in "queue_items[][position]", with: position
+    end
+  end
+
+  def add_video_to_queue(video)
     visit home_path
-    find("a[href='/videos/#{friends.id}']").click
+    find("a[href='/videos/#{video.id}']").click
     click_link('+ My Queue')
+  end
 
-    visit home_path
-    find("a[href='/videos/#{breaking_bad.id}']").click
-    click_link('+ My Queue')
+  def expect_video_to_be_in_queue(video)
+    expect(page).to have_content "#{video.title}"
+  end
 
-    within(:xpath, "//tr[contains(.,'#{fringe.title}')]") do
-      fill_in "queue_items[][position]", with: 2
-    end
-
-    within(:xpath, "//tr[contains(.,'#{friends.title}')]") do
-      fill_in "queue_items[][position]", with: 3
-    end
-
-    within(:xpath, "//tr[contains(.,'#{breaking_bad.title}')]") do
-      fill_in "queue_items[][position]", with: 1
-    end
-
+  def expect_link_not_to_be_seen(link)
+    expect(page).not_to have_content(link)
+  end
+  
+  def update_queue
     click_button('Update Instant Queue')
-
-    expect(find(:xpath, "//tr[contains(.,'#{breaking_bad.title}')]//input[@type='text']").value).to eq('1')
-
-
-    expect(find(:xpath, "//tr[contains(.,'#{fringe.title}')]//input[@type='text']").value).to eq('2')
-
-    expect(find(:xpath, "//tr[contains(.,'#{friends.title}')]//input[@type='text']").value).to eq('3')
   end
 end
