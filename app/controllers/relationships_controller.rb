2350036cb6e0712daf14e27f3033bf9dd0925d5f
Liam Sean Brady <liamseanbrady@gmail.com>
2015-07-22 13:09:35 +0100

    Create relationship

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,6 +5,13 @@ class RelationshipsController < ApplicationController
     @relationships = current_user.following_relationships
   end
 
+  def create
+    leader = User.find(params[:leader_id])
+    Relationship.create(follower_id: current_user.id, leader_id: params[:leader_id]) if current_user.can_follow?(leader)
+
+    redirect_to people_path
+  end
+
   def destroy
     relationship = Relationship.find(params[:id])
     relationship.destroy if relationship.follower == current_user
