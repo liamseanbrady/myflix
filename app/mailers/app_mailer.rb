e98e2a481c25314322e0304b6660ba7a566cb766
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-28 17:23:13 +0100

    Undo check on staging environment

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,26 +1,18 @@
 class AppMailer < ActionMailer::Base
+  default from: 'no-reply@myflix.com'
+
   def send_welcome_email(user)
     @user = user
-    send_email('ingin@example.com', user.email, 'Welcome to MyFlix')
+    mail to: user.email, subject: 'Welcome to MyFlix'
   end
 
   def send_reset_password_email(user)
     @user = user
-    send_email('ingin@example.com', user.email, 'You can now reset your password')
+    mail to: user.email, 'You can now reset your password'
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    send_email('ingin@example.com', invitation.recipient_email, invitation.message)
-  end
-
-  private 
-
-  def send_email(sender, recipient, subject)
-    if Rails.env.staging?
-      mail from: sender, to: ENV[TEST_EMAIL], subject: subject
-    else
-      mail from: sender, to: recipient, subject: subject
-    end
+    mail to: invitation.recipient_email, subject: invitation.message
   end
 end
