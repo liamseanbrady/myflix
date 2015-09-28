eb645e67b0bb6f5e0fe004b5b98b602ad8807d82
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-28 23:41:21 +0100

    Change keyword arguments to normal arguments

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,22 +3,22 @@ class AppMailer < ActionMailer::Base
 
   def send_welcome_email(user)
     @user = user
-    send_mail to: user.email, subject: 'Welcome to MyFlix'
+    send_mail user.email, 'Welcome to MyFlix'
   end
 
   def send_reset_password_email(user)
     @user = user
-    send_mail to: user.email, subject: 'You can now reset your password'
+    send_mail user.email, 'You can now reset your password'
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    send_mail to: invitation.recipient_email, subject: invitation.message
+    send_mail invitation.recipient_email, invitation.message
   end
 
   private
 
-  def send_mail(to: recipient, subject: subject)
+  def send_mail(recipient, subject)
     if Rails.env.staging?
       mail to: ENV[TEST_EMAIL], subject: subject
     else
