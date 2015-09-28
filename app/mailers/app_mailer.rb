fe4023a3f77c83538a3dbe7b6edde151b510980a
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-28 23:37:25 +0100

    Send to test email in staging environment

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,16 +3,26 @@ class AppMailer < ActionMailer::Base
 
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, subject: 'Welcome to MyFlix'
+    send_mail to: user.email, subject: 'Welcome to MyFlix'
   end
 
   def send_reset_password_email(user)
     @user = user
-    mail to: user.email, subject: 'You can now reset your password'
+    send_mail to: user.email, subject: 'You can now reset your password'
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    mail to: invitation.recipient_email, subject: invitation.message
+    send_mail to: invitation.recipient_email, subject: invitation.message
+  end
+
+  private
+
+  def send_mail(to: recipient, subject: subject)
+    if Rails.env.staging?
+      mail to: ENV[TEST_EMAIL], subject: subject
+    else
+      mail to: recipient, subject: subject
+    end
   end
 end
