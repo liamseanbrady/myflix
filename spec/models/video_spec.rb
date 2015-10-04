48b57c0f3815fef2adca5b9921f2505f54e58dff
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-04 22:38:54 +0100

    Fix tests to use Fabrication instead of creating real objects

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -8,47 +8,36 @@ describe Video do
 
   describe '.search_by_title' do
     it 'returns an empty array if there is no match' do
-      fringe = Video.create(title: 'Fringe',
-                            description: 'A sci-fi show')
-      back_to_the_future = Video.create(title: 'Back to the Future',
-                                        description: 'A good movie')
+      fringe = Fabricate(:video, title: 'Fringe')
+      back_to_the_future = Fabricate(:video, title: 'Futurama')
 
       expect(Video.search_by_title('hello')).to eq([])
     end
     
     it 'returns an array of one video for one match' do
-      fringe = Video.create(title: 'Fringe',
-                            description: 'A sci-fi show')
-      back_to_the_future = Video.create(title: 'Back to the Future',
-                                        description: 'A good movie')
+      fringe = Fabricate(:video, title: 'Fringe')
+      back_to_the_future = Fabricate(:video, title: 'Futurama')
 
       expect(Video.search_by_title('Fringe')).to eq([fringe])
     end
     
     it 'returns an array of one video for a partial match' do
-      fringe = Video.create(title: 'Fringe',
-                            description: 'A sci-fi show')
-      back_to_the_future = Video.create(title: 'Back to the Future',
-                                        description: 'A good movie')
+      fringe = Fabricate(:video, title: 'Fringe')
+      back_to_the_future = Fabricate(:video, title: 'Futurama')
 
       expect(Video.search_by_title('ring')).to eq([fringe])
     end
     
     it 'returns an array of all matches ordered by created_at' do
-      fringe = Video.create(title: 'Fringe',
-                            description: 'A sci-fi show',
-                            created_at: 1.day.ago)
-      back_to_the_future = Video.create(title: 'Back to the Future',
-                                        description: 'A good movie')
+      fringe = Fabricate(:video, title: 'Fringe')
+      back_to_the_future = Fabricate(:video, title: 'Back to the Future')
 
       expect(Video.search_by_title('F')).to eq([back_to_the_future, fringe])
     end
 
     it 'returns an empty array for a seatch with an empty string' do
-      fringe = Video.create(title: 'Fringe',
-                            description: 'A sci-fi show')
-      back_to_the_future = Video.create(title: 'Back to the Future',
-                                        description: 'A good movie')
+      fringe = Fabricate(:video, title: 'Fringe')
+      back_to_the_future = Fabricate(:video, title: 'Back to the Future')
 
       expect(Video.search_by_title('')).to eq([])
     end
