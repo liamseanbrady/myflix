bd3e3aa6cb1b56848150baa47ecc2f2965c83cff
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-04 20:12:32 +0100

    Make users controller tests read more like use cases

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,7 +8,7 @@ describe UsersController do
   end
 
   describe 'POST create' do
-    context 'valid input' do
+    context 'with valid input' do
       it 'creates a user' do
         post :create, user: Fabricate.attributes_for(:user)
           
@@ -48,43 +48,44 @@ describe UsersController do
 
       expect(invitation.reload.token).to be_nil
     end
+
+    context 'with invalid input' do
+      it 'does not create a user' do
+        post :create, user: { email: 'anon@example.com' }
+
+        expect(User.count).to eq(0)
+      end
+
+      it 'renders the view template' do
+        post :create, user: { email: 'anon@example.com' }
+
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'when sending email' do
+      before { ActionMailer::Base.deliveries.clear }
+      
+      it 'sends out email to the user with valid inputs' do
+        post :create, user: { full_name: 'Alice', email: 'alice@example.com', password: 'password' }
+
+        expect(ActionMailer::Base.deliveries).not_to be_empty
+      end
+
+      it "sends out email containing the user's name with valid inputs" do
+        post :create, user: { full_name: 'Alice', email: 'alice@example.com', password: 'password' }
+
+        expect(ActionMailer::Base.deliveries.last.body).to include("Alice")
+      end
+
+      it 'does not send out email with invalid inputs' do
+        post :create, user: { email: 'alice@example.com' }
+
+        expect(ActionMailer::Base.deliveries).to be_empty
+      end
+    end
   end
 
-  context 'invalid input' do
-    it 'does not create a user' do
-      post :create, user: { email: 'anon@example.com' }
-
-      expect(User.count).to eq(0)
-    end
-
-    it 'renders the view template' do
-      post :create, user: { email: 'anon@example.com' }
-
-      expect(response).to render_template :new
-    end
-  end
-
-  context 'sending email' do
-    before { ActionMailer::Base.deliveries.clear }
-    
-    it 'sends out email to the user with valid inputs' do
-      post :create, user: { full_name: 'Alice', email: 'alice@example.com', password: 'password' }
-
-      expect(ActionMailer::Base.deliveries).not_to be_empty
-    end
-
-    it "sends out email containing the user's name with valid inputs" do
-      post :create, user: { full_name: 'Alice', email: 'alice@example.com', password: 'password' }
-
-      expect(ActionMailer::Base.deliveries.last.body).to include("Alice")
-    end
-
-    it 'does not send out email with invalid inputs' do
-      post :create, user: { email: 'alice@example.com' }
-
-      expect(ActionMailer::Base.deliveries).to be_empty
-    end
-  end
 
   describe 'GET show' do
     it_behaves_like 'requires sign in' do
