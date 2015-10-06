ae4ad2e819360253aaf1706d64e95d96542ce715
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-06 01:24:57 +0100

    Fix failure point in test - update name of submit button for form

--- spec/features/user_invites_friend_spec.rb
+++ spec/features/user_invites_friend_spec.rb
@@ -29,7 +29,7 @@ feature 'User invites friend' do
     current_email.click_link 'Accept this invitation'
     fill_in 'Password', with: 'password'
     fill_in 'Full Name', with: 'Bob Doe'
-    click_button 'Submit'
+    click_button 'Register and Pay'
   end
 
   def friend_signs_in
