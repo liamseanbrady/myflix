cf80b52538d3e444a8c492ad76937fa5e034eb91
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-07 22:54:54 +0100

    Set the appropriate Capybara server port and make sure RSpec doesn't do its clean up after each spec with transactions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Capybara.server_port = 52662
+
 VCR.configure do |c|
   c.default_cassette_options = { 
     :record => :new_episodes, 
@@ -50,7 +52,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -90,9 +92,9 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :truncation
   end
 
-# config.before(:each) do
-#   DatabaseCleaner.start
-# end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
   config.after(:each) do
     DatabaseCleaner.clean
