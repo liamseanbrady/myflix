00f1604752af773fa752c25b8fbd4024543080e4
Liam Sean Brady <liamseanbrady@gmail.com>
2015-10-19 21:21:08 +0100

    Move user registration process to UserRegistration service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,26 +8,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.valid?
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-      charge = StripeWrapper::Charge.create(
-        :amount => 999,
-        :card => params[:stripeToken],
-        :description => "Sign up charge for #{@user.email}"
-      )
+    result = UserRegistration.new(@user).register(params[:stripeToken], params[:invitation_token])
 
-      if charge.successful?
-        @user.save
-        handle_invitation
-        AppMailer.send_welcome_email(@user).deliver
-        flash[:success] = 'Thank you for registering with MyFlix. Please sign in now'
-        redirect_to sign_in_path
-      else
-        flash.now[:danger] = charge.error_message
-        render :new
-      end
+    if result.successful?
+      flash[:success] = 'Thank you for registering with MyFlix. Please sign in now'
+      redirect_to sign_in_path
     else
-      flash.now[:danger] = 'Invalid user information. Please check the errors below'
+      flash[:danger] = result.error_message
       render :new
     end
   end
@@ -53,12 +40,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :password, :full_name)
   end
 
-  def handle_invitation
-    if params[:invitation_token].present?
-      invitation = Invitation.find_by(token: params[:invitation_token])
-      @user.follow(invitation.inviter)
-      invitation.inviter.follow(@user)
-      invitation.update_column(:token, nil)
-    end
-  end
 end
