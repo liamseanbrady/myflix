858836eae06c368dde5e33dd2dea34c94b1bb3fe
Liam Sean Brady <liamseanbrady@gmail.com>
2015-11-02 17:20:52 +0000

    Handle failed payments

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,4 +5,9 @@ StripeEvent.setup do
     user = User.find_by(customer_token: event.data.object.customer)
     Payment.create(reference_id: event.data.object.id, user: user, amount: event.data.object.amount)
   end
+
+  subscribe 'charge.failed' do |event|
+    user = User.find_by(customer_token: event.data.object.customer)
+    user.deactivate!
+  end
 end
