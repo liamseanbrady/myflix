86d852b8f2a0bac0b28b7e8c8efdc5de7d15847e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-11-04 15:52:44 +0000

    Add .search method to search elasticsearch and returns records where appropriate

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,7 @@
 class Video < ActiveRecord::Base
+  include Elasticsearch::Model
+  index_name ['myflix', Rails.env].join('_')
+
   belongs_to :category  
   has_many :reviews
 
@@ -14,5 +17,13 @@ class Video < ActiveRecord::Base
 
   def rating
     reviews.average(:rating).round(1) if reviews.any?
+  end
+
+  def as_indexed_json(options = {})
+    as_json(only: [:title, :description])
+  end
+
+  def self.search(term)
+    __elasticsearch__.search(term).records.to_a
   end
 end
